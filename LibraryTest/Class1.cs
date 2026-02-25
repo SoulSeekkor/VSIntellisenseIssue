4b1863a49b7592c276dbae910256a8d5cfdbba5d
Michael Ewert <mewert@eojohnson.com>
2026-02-25 14:49:11 -0600

    Added enum to library.

--- LibraryTest/Class1.cs
+++ LibraryTest/Class1.cs
@@ -4,6 +4,13 @@ namespace LibraryTest
 {
     public class Class1
     {
-
+        public enum enuTest
+        {
+            value1 = 0,
+            value2 = 1,
+            value3 = 2,
+            value4 = 3,
+            value5 = 4
+        }
     }
 }
